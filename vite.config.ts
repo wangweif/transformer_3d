4e915518cb4f1c11ff69658141a674d856b5899d
wangweif <wangweif@github.com>
2026-03-10 10:46:29 +0800

    添加 base 路径配置，修复 index.html 脚本引用

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
+      base: '/apps/transformer_3d/',      server: {
         port: 3000,
         host: '0.0.0.0',
       },
